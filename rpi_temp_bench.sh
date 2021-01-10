5f66053483ad056138c31defaf2102879919fa7d
mstroh76 <m.stroh76@gmail.com>
2021-01-10 13:01:55 +0100

    stress-ng

--- rpi_temp_bench.sh
+++ rpi_temp_bench.sh
@@ -1,13 +1,13 @@
 bcmstat.sh > rpi_$1.txt &
 PID=$!
 sleep 10
-stress --cpu 1 --timeout 420s
+stress-ng --cpu 1 --timeout 420s
 sleep 180
-stress --cpu 2 --timeout 420s
+stress-ng --cpu 2 --timeout 420s
 sleep 180
-stress --cpu 3 --timeout 420s
+stress-ng --cpu 3 --timeout 420s
 sleep 180
-stress --cpu 4 --timeout 420s
+stress-ng --cpu 4 --timeout 420s
 sleep 180
 echo kill $PID
 kill -KILL $PID
